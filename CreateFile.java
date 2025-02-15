9844a50de7092ce7d81f743341635aba26b3dc84
niha611 <nihashaikh0502@gmail.com>
2025-02-15 19:43:19 +0530

    Add files via upload

--- CreateFile.java
+++ CreateFile.java
@@ -1,24 +1,30 @@
-package FILEOPERATIONS;
-
+package JavaTask;
 import java.io.File;
 import java.io.IOException;
 
-public class CreateFile {
-	
-	public static void main(String args[]) {  
-        try {  
-                // Creating an object of a file  
-                File f0 = new File("D:FileOperationExample.txt");   
-                if (f0.createNewFile()) {  
-                           System.out.println("File " + f0.getName() + " is created successfully.");  
-                } else {  
-                           System.out.println("File is already exist in the directory.");  
-                }  
-              } catch (IOException exception) {  
-                       System.out.println("An unexpected error is occurred.");  
-                       exception.printStackTrace();  
-           }   
- }  
 
+public class CreateFile {
+	 public static void main(String[] args)
+	    {
+	          // Creating the File also
+	          // Handling Exception
+	        try {
+	            File Obj = new File("myfile.txt");
+	            
+	              // Creating File
+	              if (Obj.createNewFile()) {
+	                System.out.println("File created: " + Obj.getName());
+	            }
+	            else {
+	                System.out.println("File already exists.");
+	            }
+	        }
+	      
+	          // Exception Thrown
+	        catch (IOException e) {
+	            System.out.println("An error has occurred.");
+	            e.printStackTrace();
+	        }
+	    }
 
 }
---
+++ WriteFile.java
@@ -0,0 +1,31 @@
+package JavaTask;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+	
+	
+	public static void main(String[] args)
+    {
+        // Writing Text File also
+        // Exception Handling
+        try {
+
+            FileWriter Writer = new FileWriter("myfile.txt");
+
+            // Writing File
+            Writer.write("Files in Java are seriously good!!");
+            Writer.close();
+            
+            System.out.println("Successfully written.");
+        }
+
+        // Exception Thrown
+        catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+
+}
